4e0dbce2c5316f74271a08c78b843cdabe0728a8
Ashfaq Akhtar <ashfaqakhtar@live.com>
2021-09-12 07:04:54 +0530

    Added Line

--- AnimalQuiz/main.py
+++ AnimalQuiz/main.py
@@ -9,9 +9,9 @@ def check_guess(guess, answer):
             still_guessing = False
         else:
             if attempt < 2:
-                guess = input("Sorry Wrong Answer chance left: ")
+                guess = input("Sorry Wrong Answer Try Again: ")
         attempt = attempt + 1
-    if attempt == 2:
+    if attempt == 3:
         print("Correct answer is " + answer)
 
 
@@ -19,12 +19,15 @@ score = 0
 name = input("Enter Your Name: ")
 print(f"Welcome {name} to Animal Quiz !! Let's Start")
 
+guess0 =input("Which of the following is fish ? \n \
+A)Whale\n B)Dolphin\n C)Shark\n D)Squid.\nType A,B,C, or D: ")
+check_guess(guess0, 'C')
 guess1 = input("Which Bear lives at North Pole?: ")
 check_guess(guess1, 'Polar Bear')
 guess2 = input("Which is the fastest land Animal? : ")
 check_guess(guess2, 'Cheetah')
 guess3 = input("Which is largest Animal?: ")
-check_guess(guess3, 'Blue Wheel')
+check_guess(guess3, 'Blue Whale')
 guess4 = input("which Animal is called King of Forest?: ")
 check_guess(guess4, 'Lion')
 guess5 = input("Which is Tallest Animal?: ")
